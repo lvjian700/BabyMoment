3000fa652b3b890c126e94d3b2a5a7a942bf9a85
iossocket <avx302@gmail.com>
2016-08-24 14:08:49 +0800

    add basic component

--- BabyMoment/ViewController.swift
+++ BabyMoment/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    self.tableView.dataSource = self
+    self.tableView.estimatedRowHeight = 314
+    self.tableView.rowHeight = UITableViewAutomaticDimension
   }
+}
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+extension ViewController: UITableViewDataSource {
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
   }
-
-
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    return cell
+  }
 }
 

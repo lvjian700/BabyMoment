21b97851c271fc21a7a3b4ebbe93c3e490a66ec0
iossocket <avx302@gmail.com>
2016-08-24 14:17:15 +0800

    add moment cell

---
+++ BabyMoment/MomentCell.swift
@@ -0,0 +1,23 @@
+//
+//  MomentCell.swift
+//  BabyMoment
+//
+//  Created by Xueliang Zhu on 8/24/16.
+//  Copyright © 2016 kotlinchina. All rights reserved.
+//
+
+import UIKit
+
+class MomentCell: UITableViewCell {
+    
+    @IBOutlet weak var day: UILabel!
+    @IBOutlet weak var yearMonth: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var heroImage: UIImageView!
+    @IBOutlet weak var comments: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- BabyMoment/ViewController.swift
+++ BabyMoment/ViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.tableView.dataSource = self
-    self.tableView.estimatedRowHeight = 314
-    self.tableView.rowHeight = UITableViewAutomaticDimension
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.tableView.estimatedRowHeight = 314
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+    }
 }
 
 extension ViewController: UITableViewDataSource {
   
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5
-  }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
   
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-    return cell
-  }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! MomentCell
+        return cell
+    }
 }
 

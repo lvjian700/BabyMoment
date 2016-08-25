cc351bef0d57574d4062f16c3dfb7d059d2ab393
xing.zhou <xing.zhou@rea-group.com>
2016-08-25 14:09:22 +0800

    Add BirthdayViewController

---
+++ BabyMoment/BirthdayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BirthdayViewController.swift
+//  BabyMoment
+//
+//  Created by Xing Zhou on 8/25/16.
+//  Copyright © 2016 kotlinchina. All rights reserved.
+//
+
+import UIKit
+
+class BirthdayViewController: UIViewController {
+
+    var birthday :String?
+    
+    @IBOutlet weak var birthday_picker: UIDatePicker!
+    
+    @IBAction func submit_birthday(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd MMM yyy"
+        
+        birthday = dateFormatter.stringFromDate(birthday_picker.date)
+        print(birthday!)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        birthday_picker.maximumDate = NSDate()
+        // Do any additional setup after loading the view.
+    }
+}

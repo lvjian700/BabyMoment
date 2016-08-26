1c297a0786d701d0cf3ae4df2bef80567dd9dfd8
iossocket <avx302@gmail.com>
2016-08-26 10:15:53 +0800

    save gender

--- BabyMoment/ChooseGenderViewController.swift
+++ BabyMoment/ChooseGenderViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+enum Gender: Int {
+    case Girl, Boy
+}
+
 class ChooseGenderViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -18,15 +22,23 @@ class ChooseGenderViewController: UIViewController {
     
     @IBAction func chooseGirl(sender: AnyObject) {
         showNextPage()
+        setGender(.Girl)
     }
     
     @IBAction func chooseBoy(sender: AnyObject) {
         showNextPage()
+        setGender(.Boy)
     }
     
     private func showNextPage() {
         let chooseDateVC =
             UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ChooseBirthdayViewController") as! ChooseBirthdayViewController
         navigationController?.pushViewController(chooseDateVC, animated: true)
+    }
+    
+    private func setGender(gender: Gender) {
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setInteger(gender.rawValue, forKey: "Gender")
+        userDefaults.synchronize()
     }
 }

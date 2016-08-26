429ca12811bc5cfc9f972f57b9ae95b7d4cd59fb
iossocket <avx302@gmail.com>
2016-08-26 10:24:36 +0800

    save birthday

--- BabyMoment/ChooseBirthdayViewController.swift
+++ BabyMoment/ChooseBirthdayViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class ChooseBirthdayViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.hidden = true
     }
     
     @IBAction func chooseDate(sender: AnyObject) {
+        setBirthday(datePicker.date)
+        showNextPage()
+    }
+    
+    private func showNextPage() {
         let app = UIApplication.sharedApplication().delegate as! AppDelegate
         app.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("TabBarVC") as! TabBarVC
     }
     
+    private func setBirthday(date: NSDate) {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setObject(formatter.stringFromDate(datePicker.date), forKey: "Birthday")
+        userDefaults.synchronize()
+    }
 }

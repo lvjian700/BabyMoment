ed3548a8a8e15034f21ddb286dde4470e0564f1b
Jian Lv <jlv@thoughtworks.com>
2016-09-02 07:38:27 +0800

    Install Realm for swift

---
+++ BabyMoment/BabyProfile.swift
@@ -0,0 +1,19 @@
+//
+//  BabyProfile.swift
+//  BabyMoment
+//
+//  Created by twcn  on 9/1/16.
+//  Copyright © 2016 kotlinchina. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class BabyProfile: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
